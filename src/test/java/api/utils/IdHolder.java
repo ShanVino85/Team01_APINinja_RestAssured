31825bf6eb436b1e0e66484583e4b101ae1a6a77
Vinothini R <106883359+ShanVino85@users.noreply.github.com>
2024-08-19 18:41:01 -0400

    Update IdHolder.java

--- src/test/java/api/utils/IdHolder.java
+++ src/test/java/api/utils/IdHolder.java
@@ -2,20 +2,20 @@ package api.utils;
 
 public class IdHolder {
 
-	public static String token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJUZWFtMS5hZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE3MjQwNzUyNzksImV4cCI6MTcyNDEwNDA3OX0.Wo-8NR4eVagRJ0Bk6rVxzevL8p_fg5zdC6QtetTqOCOwskVuKGABlyYCtzcFK1upxK80pAWqbxqfPPstK-4v2w";
-	public static String Dieticiantoken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJNY2xlYW4xMDZAZ21haWwuY29tIiwiaWF0IjoxNzI0MDc1MzY3LCJleHAiOjE3MjQxMDQxNjd9.W4Y1DayidYDvf4n0I83Kc_cl6ep14br8_s1IAptXVdrA0GG0vIQsBgA4qL82K44QLlfrd325pK95kjQtkaSk0g";
+	public static String Admintoken;
+	public static String DieticianId;
+	public static String Dieticiantoken;
+	public static String patientId1;
+	public static String patientId2;
+	public static String patientEmail;
+	public static String Patienttoken;
 	
-	 public static String testName;
-	 
-	 public static String FileID;
-	 
-	 public static String Patienttoken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZGFtMTIyQGdtYWlsLmNvbSIsImlhdCI6MTcyNDA3NTQwOSwiZXhwIjoxNzI0MTA0MjA5fQ.hiLzHU2vP-6ZtxZiJACA4SMMaqUfkxEvUm-y04l0N0ETuRLyolF00Px7jdJGTVXOAsXZrElX0GOqjcGJCTU09A";
-	 
-	 
+
 	
+	//Invalid Variables
+	public static String invalidpatientId="P30";
+	public static String invalidpatientEmail="DSam@gmail.com";
 	
-	
-    
  
  	
      
