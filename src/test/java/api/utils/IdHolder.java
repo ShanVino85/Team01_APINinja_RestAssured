33a47fcdddca9516255c1ede792a8ab8dcbf0736
parisneha1412 <snehaprayag1412@gmail.com>
2024-08-19 11:20:06 -0400

    Update IdHolder.java

--- src/test/java/api/utils/IdHolder.java
+++ src/test/java/api/utils/IdHolder.java
@@ -2,14 +2,14 @@ package api.utils;
 
 public class IdHolder {
 
-	public static String token="";
-	public static String Dieticiantoken="";
+	public static String token=;
+	public static String Dieticiantoken=;
 	
 	 public static String testName;
 	 
 	 public static String FileID;
 	 
-	 public static String Patienttoken="";
+	 public static String Patienttoken=;
 	 
 	 
 	
