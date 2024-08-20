c85d4a86dabaead25a009fb3f4c850cdf307acd2
shreyadas1421 <shreyadas1421@gmail.com>
2024-08-20 08:38:28 -0400

    Sync with Master IdHolder

--- src/test/java/api/utils/IdHolder.java
+++ src/test/java/api/utils/IdHolder.java
@@ -2,12 +2,28 @@ package api.utils;
 
 public class IdHolder {
 
-	public static String token;
+public static String Admintoken;
+	public static String DieticianId;
+	public static String Dieticiantoken;
+	public static String patientId1;
+	public static String patientId2;
+	public static String patientEmail;
+	public static String Patienttoken;
+        public static String testName; 
+	 public static String FileID;	
+
+
+        public static String token;
 	
 	public static String dietitianEmail;
 	public static String dietitianpass;
 	public static String dietitianToken;
 	public static String dietitianID;
-	public static String dietitianID1;     
+	public static String dietitianID1;   
+
+//Invalid Variables
+	public static String invalidpatientId="P30";
+	public static String invalidpatientEmail="DSam@gmail.com";
+	  
      
 }
